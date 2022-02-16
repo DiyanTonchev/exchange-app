d58dd3191b3c5d7c604024583255224584df9fe0
diyantonchev <diyantonchev@gmail.com>
2022-02-16 17:44:31 +0200

    Fixed http proxy middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,53 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = (app) => {
+module.exports = function(app) {
   app.use(
-    createProxyMiddleware('api.binance.com/api/v3/', {
-      target: 'http://localhost:3000/',
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
       changeOrigin: true,
-      headers: {
-        Connection: 'keep-alive'
-      }
     })
   );
-  
-  app.use(
-    createProxyMiddleware('api-pub.bitfinex.com/v2**', {
-      target: 'http://localhost:3000/',
-      changeOrigin: true,
-      headers: {
-        Connection: 'keep-alive'
-      }
-    })
-  );
-
-  app.use(
-    createProxyMiddleware('api.huobi.pro/', {
-      target: 'http://localhost:3000/',
-      changeOrigin: true,
-      headers: {
-        Connection: 'keep-alive'
-      }
-    })
-  );
-
-  app.use(
-    createProxyMiddleware('api.kraken.com/0/public/Ticker', {
-      target: 'http://localhost:3000/',
-      changeOrigin: true,
-      headers: {
-        Connection: 'keep-alive'
-      }
-    })
-  );
-
-  app.use(
-    createProxyMiddleware('api.kraken.com/0/public/Trades', {
-      target: 'localhost:3000/',
-      changeOrigin: true,
-      headers: {
-        Connection: 'keep-alive'
-      }
-    })
-  );
-}
+};
